6ee6424727a52707491149c8d89539018cbcccb3
Geo2406 <>
2020-12-17 23:12:21 +0400

    big update

---
+++ Assets/Scripts/Enemyblob Scripts/Bullet.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+
+    public float speed;
+    public Transform Player;
+    Vector2 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+
+
+
+
+        target = new Vector2(Player.position.x, Player.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if(transform.position.x == target.x && transform.position.y == target.y)
+        {
+            Destroybullet();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Destroybullet();
+        }
+    }
+
+   
+
+
+    void Destroybullet()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Enemyblob Scripts/Enemyshoot.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemyshoot : MonoBehaviour
+{
+
+
+    public float starttimebtwshot;
+    private float timebtwshots;
+    public GameObject projectile;
+
+    bool istouching;
+    public float radius;
+    public Transform center;
+    public LayerMask Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timebtwshots = starttimebtwshot;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        istouching = Physics2D.OverlapCircle(center.position, radius, Player);
+
+        
+        void OnGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(center.position, radius);
+        }
+        
+        
+      if(istouching)
+        {
+            if (timebtwshots < 0)
+            {
+                Instantiate(projectile, transform.position, Quaternion.identity);
+
+                timebtwshots = starttimebtwshot;
+            }
+            else
+            {
+                timebtwshots -= Time.deltaTime;
+            }
+        }
+        
+        
+    }
+
+}
