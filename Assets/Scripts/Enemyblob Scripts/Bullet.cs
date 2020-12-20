fa6b1051b9860bfd3d825489233ee1ae2769305f
Geo2406 <>
2020-12-21 02:26:52 +0400

    yes

--- Assets/Scripts/Enemyblob Scripts/Bullet.cs
+++ Assets/Scripts/Enemyblob Scripts/Bullet.cs
@@ -32,7 +32,14 @@ public class Bullet : MonoBehaviour
         }
     }
 
-    
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Platform")
+        {
+            Destroybullet();
+        }
+
+    }
 
    
 
