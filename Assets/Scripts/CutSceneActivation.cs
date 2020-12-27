e573d6614b04b5568170a7e0e26ebf30d292d25d
Geo2406 <>
2020-12-27 20:17:44 +0400

    yes

--- Assets/Scripts/CutSceneActivation.cs
+++ Assets/Scripts/CutSceneActivation.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
 
 public class CutSceneActivation : MonoBehaviour
 {
 
     public GameObject CutScene1;
     public GameObject text;
+
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -24,8 +29,19 @@ public class CutSceneActivation : MonoBehaviour
         if(other.gameObject.tag == "Player")
         {
             CutScene1.SetActive(true);
-            Time.timeScale = 0;
+            
             text.SetActive(false);
+            
+
+              
+            
+               
+            
         }
+    }
+
+    public void nextlevel()
+    {
+        SceneManager.LoadScene(1);
     }
 }
