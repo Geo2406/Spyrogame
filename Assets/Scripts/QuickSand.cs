bb6cb8a0aff1b6b5c10201e1cddcbe71a1cd73da
Geo2406 <>
2021-01-03 02:22:51 +0400

    level3 mechanic

--- Assets/Scripts/QuickSand.cs
+++ Assets/Scripts/QuickSand.cs
@@ -10,10 +10,14 @@ public class QuickSand : MonoBehaviour
     public float radius;
     public Transform Center;
     bool istouching;
+
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
+        rb = Player.GetComponent<Rigidbody2D>();
+  
     }
 
     // Update is called once per frame
@@ -21,16 +25,24 @@ public class QuickSand : MonoBehaviour
     {
         istouching = Physics2D.OverlapCircle(Center.position, radius, player);
 
+        void OnGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(Center.position, radius);
+        }
 
-        if(istouching)
+        if (istouching)
         {
             Player.GetComponent<Movment>().speed = 2f;
             Player.GetComponent<Movment>().jumpforce = 1f;
+            rb.gravityScale = 0.5f;
 
         }else
         {
             Player.GetComponent<Movment>().speed = 6f;
             Player.GetComponent<Movment>().jumpforce = 4f;
+            rb.gravityScale = 1f;
+            
         }
     }
 }
